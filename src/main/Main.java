52b6d52a64fdc0df87f77510ac23283dad06bbcf
Momonan0412 <chua.avrilnigel0412@gmail.com>
2023-11-22 11:05:46 +0800

    hello

--- src/main/Main.java
+++ src/main/Main.java
@@ -6,5 +6,6 @@ public class Main {
     public static void main(String[] args) throws IOException {
         GamePanel gamePanel = new GamePanel();
         gamePanel.startGameThread();
+        System.out.print("Hello World");
     }
 }
